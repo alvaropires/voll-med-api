c9a2dc0729a8016f5bbbb76e5e4be028bd896c79
Alvaro <alvaropires.eng@hotmail.com>
2023-05-24 09:55:05 -0300

    criado método GET para MedicoController e métodos POST e GET para PacienteController

--- api/src/main/java/med/voll/api/controller/PacienteController.java
+++ api/src/main/java/med/voll/api/controller/PacienteController.java
@@ -5,7 +5,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.data.web.PageableDefault;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,27 +12,26 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import jakarta.validation.Valid;
-import med.voll.api.medico.DadosCadastroMedico;
-import med.voll.api.medico.DadosListagemMedico;
-import med.voll.api.medico.Medico;
-import med.voll.api.medico.MedicoRepository;
+import med.voll.api.paciente.DadosCadastroPaciente;
+import med.voll.api.paciente.DadosListagemPaciente;
+import med.voll.api.paciente.Paciente;
+import med.voll.api.paciente.PacienteRepository;
 
 @RestController
-@RequestMapping("/medicos")
-public class MedicoController {
+@RequestMapping("/pacientes")
+public class PacienteController {
 	
 	@Autowired
-	private MedicoRepository medicoRepository;
+	private PacienteRepository pacienteRepository;
 	
 	@PostMapping
-	@Transactional
-	public void cadastrar(@RequestBody @Valid DadosCadastroMedico dados) {
-		
-		medicoRepository.save(new Medico(dados));
+	public void cadastrar(@RequestBody @Valid DadosCadastroPaciente dados) {
+		pacienteRepository.save(new Paciente(dados));
 	}
 	
 	@GetMapping
-	public Page<DadosListagemMedico> listar(@PageableDefault(page = 0, size=10, sort = {"nome"}, direction = Direction.ASC) Pageable paginacao){
-		return medicoRepository.findAll(paginacao).map(DadosListagemMedico::new);
+	public Page<DadosListagemPaciente> listar(@PageableDefault(page = 0, size = 10, sort = {"nome"}, direction = Direction.ASC) Pageable paginacao){
+		return pacienteRepository.findAll(paginacao).map(DadosListagemPaciente::new);
 	}
+
 }
