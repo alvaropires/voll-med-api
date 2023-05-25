c75ce30bcfec4d660c42b319dce338ada885abe5
Alvaro Pires Santos <alvaropires.eng@hotmail.com>
2023-05-24 22:39:37 -0300

    implementado método atualiza na classe MedicoController

--- api/src/main/java/med/voll/api/controller/MedicoController.java
+++ api/src/main/java/med/voll/api/controller/MedicoController.java
@@ -8,11 +8,13 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import jakarta.validation.Valid;
+import med.voll.api.medico.DadosAtualizacaoMedico;
 import med.voll.api.medico.DadosCadastroMedico;
 import med.voll.api.medico.DadosListagemMedico;
 import med.voll.api.medico.Medico;
@@ -35,5 +37,12 @@ public class MedicoController {
 	@GetMapping
 	public Page<DadosListagemMedico> listar(@PageableDefault(page = 0, size=10, sort = {"nome"}, direction = Direction.ASC) Pageable paginacao){
 		return medicoRepository.findAll(paginacao).map(DadosListagemMedico::new);
+	}
+	
+	@PutMapping
+	@Transactional
+	public void atualizar(@RequestBody @Valid DadosAtualizacaoMedico dados) {
+		Medico medico = medicoRepository.findById(dados.id()).orElseThrow();
+		medico.atualizarInformacoes(dados);
 	}
 }
