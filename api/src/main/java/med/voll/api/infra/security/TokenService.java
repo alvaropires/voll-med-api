b938e0f538393019d1249d957fa299a01def8890
Alvaro Pires Santos <alvaropires.eng@hotmail.com>
2023-05-28 23:24:27 -0300

    implementado configurações de segurança para acesso a aplicação com utilização de filtros e validação do token JWT

--- api/src/main/java/med/voll/api/infra/security/TokenService.java
+++ api/src/main/java/med/voll/api/infra/security/TokenService.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Service;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 
 import med.voll.api.domain.usuario.Usuario;
 
@@ -32,6 +33,21 @@ public class TokenService {
 			throw new RuntimeException("Erro ao gerar token jwt", exception);
 		}
 		
+	}
+	
+	public String getSubject(String tokenJWT) {
+		
+		try {
+			var algoritimo = Algorithm.HMAC256(secret);
+		    return JWT.require(algoritimo)
+		        .withIssuer("API Voll.med")
+		        .build()
+		        .verify(tokenJWT)
+		        .getSubject();
+		        
+		} catch (JWTVerificationException exception){
+			throw new RuntimeException("Token JWT inválido ou expirado!");
+		}
 	}
 
 	private Instant dataExpiracao() {
